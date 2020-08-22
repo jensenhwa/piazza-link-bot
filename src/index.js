03b2f5c2e85d6268fc87cf8e26adebc05ac11a55
Jensen Hwa <30474284+jensenhwa@users.noreply.github.com>
2020-08-22 14:10:49 -0400

    Add username to reply message

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ slackEvents.on('message', (message) => {
       var replyMessage = {
         channel: message.channel,
         text: reply,
+        username: "Leslie Nielson",
         unfurl_links: true
       }
 
